7244b75a49a682b068a4e6b5e05e9478622d94c6
carissa morrow <carissamorrow@hotmail.com>
2019-01-07 14:43:57 -0700

    console_library

---
+++ Models/library.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+namespace console_library.Models
+{
+  public class Library
+  {
+    public string Name { get; private set; }
+    private List<Book> Books { get; set; }
+    public Library(string name)
+    {
+      Name = name;
+      Books = new List<Book>();
+    }
+    public void AddBook(Book book)
+    {
+      Books.Add(book);
+    }
+
+    public void PrintBooks()
+    {
+      for (int i = 0; i < Books.Count; i++)
+      {
+        System.Console.WriteLine($"{i + 1} {Books[i].Title} - {Books[i].Author}");
+      }
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace console_library.Models
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      var library = new Library("Welcome to the Library!");
+      Book sidewalk = new Book("Shel Silverstein", "Where the Sidewalk Ends");
+      Book thisIsANewBook = new Book("Fake Author", "Fake Title");
+      Book thisIsFakeBookTwo = new Book("Fake Author Two", "Fake Title Two");
+      library.AddBook(sidewalk);
+      library.AddBook(thisIsANewBook);
+      library.AddBook(thisIsFakeBookTwo);
+
+      Console.WriteLine(library.Name);
+      library.PrintBooks();
+      var inthelibrary = true;
+      while (inthelibrary)
+      {
+        Console.WriteLine("Welcome to " + library.Name);
+        Console.WriteLine("Here are your options:");
+        Console.WriteLine("1. Check out a book");
+        Console.WriteLine("2. Return a book");
+      }
+    }
+  }
+}
+
